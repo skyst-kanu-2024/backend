86a6527e80865266add3b57a5a774e16cb48987b
DoyunShin <doyun.shin@gmail.com>
2024-03-23 21:00:50 +0900

    fix(profile): Fix some things

--- kanu/profile/__init__.py
+++ kanu/profile/__init__.py
@@ -7,27 +7,29 @@ class Profile:
     id:str
     mbti:str
     introduce:str
+
 def setup():
     conn = kanu.database.Database()
     cursor = conn.cursor()
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS profile(
     id char(32) NOT NULL UNIQUE,
-    mbti char(4) NOT NULL,
-    introduce char(300) NOT NULL
+    mbti varchar(4) NOT NULL,
+    introduce varchar(300) NOT NULL,
+    PRIMARY KEY(id)
     )
     """)
     
 def create_profile(
-    id:kanu.user.User.id,
+    user: kanu.user.User,
     mbti:str,
     introduce:str
-) ->Profile:
+) -> Profile:
     conn = kanu.database.Database()
     cursor = conn.cursor()
     cursor.execute("""
     SELECT (id,mbti,introduce) FROM profile WHERE id = %s
-    """,id)
+    """,(user.id))
     exist = cursor.fetchone()
     if exist:
         profile = Profile()
@@ -37,21 +39,21 @@ def create_profile(
         return profile
     cursor.execute("""
     INSERT INTO profile(id,mbti,introduce) VALUES (%s,%s,%s)
-    """,(id,mbti,introduce))
+    """,(user.id,mbti,introduce))
     profile = Profile()
-    profile.id = id
+    profile.id = user.id
     profile.mbti = mbti
     profile.introduce = introduce
     return profile
     
 def get_profile(
-    id:kanu.user.User.id,
+    user: kanu.user.User
 ) -> Profile:
     conn = kanu.database.Database()
     cursor = conn.cursor()
     cursor.execute("""
     SELECT (id,mbti,introduction) FROM profile WHERE id = %s
-    """,id)
+    """,(user.id))
     oneprofile = cursor.fetchone()
     profile = Profile()
     profile.id = oneprofile[0]
